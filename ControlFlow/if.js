6f00a0f415e0a7980960d5c64cb82971173397b3
Bobby <abc@example.com>
2021-03-10 17:06:38 -0500

    3/10/2021

---
+++ ControlFlow/if.js
@@ -0,0 +1,11 @@
+*/
+-A falsy value is a value that is considered false when encountered in a boolewan AudioContext
+        -false
+        -0
+        "",  
+        -null
+        -undefined-NaN(not a number)
+
+
+        -This means that when JavaScript is expecting a boolean and it is given one of the values above, it will always evaluate to "falsy".includes
+        */
